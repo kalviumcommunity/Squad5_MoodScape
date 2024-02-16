b170168a365444cea54e629bd0a6cdd037d13227
Ram nathawat <nathawatr7@gmail.com>
2024-02-16 16:27:31 +0530

    MongoDB/Mongoose

---
+++ BacKend/Model/MoodScape.js
@@ -0,0 +1,12 @@
+const mongoose=require("mongoose");
+const MoodScapeSchema = mongoose.Schema({
+    poster: { type: String, required: true },
+    songName: { type: String, required: true },
+    artist: { type: String, required: true },
+    songLength: { type: String, required: true },
+    albumName: { type: String, required: true },
+    genre: { type: String, required: true },
+    mood: { type: String, required: true },
+});
+const MoodScapeModel=mongoose.model('MoodScape', MoodScapeModel)
+module.exports={MoodScapeModel};
--- BacKend/server.js
+++ BacKend/server.js
@@ -3,12 +3,28 @@ const app = express();
 const dotenv=require('dotenv');
 dotenv.config()
 const port = process.env.PUBLIC_PORT || 3000; 
+const {connection}=require(Database/Data.js);
+const {MoodScapeSchema }= require(/Database/database);
+const {MoodScapeModel}= require(/Model/MoodScape);
+const mongoose = require("mongoose");
+const MoodScapeData = require('./Database/database');
 
 
 app.get('/ping', (req, res) => {
   res.json({ message: 'pong' });
 });
 
+app.post('/postdata',(req,res)=>{
+  MoodScapeModel.insertMany(MoodScapeData)
+    .then((result)=>{
+      console.log('Inserted', result.length,"documents into the collection");
+      res.status(200).send('Data inserted successfully');
+    })
+    .catch((error)=>{
+      console.error('Error inserting documents',error);
+      res.status(500).send('Failed to insert data');
+    });
+});
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
